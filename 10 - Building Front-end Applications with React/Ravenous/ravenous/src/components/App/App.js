7a08754d39b27e15da3d0f7d9303ee4651137b5a
dyrits <dyrits@outlook.fr>
2020-06-21 12:14:55 +0200

    Part 2 completed.

--- 10 - Building Front-end Applications with React/Ravenous/ravenous/src/components/App/App.js
+++ 10 - Building Front-end Applications with React/Ravenous/ravenous/src/components/App/App.js
@@ -25,7 +25,7 @@ class App extends Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar />
-        <BusinessList />
+        <BusinessList businesses={businesses}/>
       </div>
     )
   }
