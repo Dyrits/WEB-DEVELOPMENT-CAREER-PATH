041daa5fc2b0822a838a613f5a971db2d760ab84
dyrits <dyrits@outlook.fr>
2020-07-06 21:04:54 +0200

    Function rewritten.

--- 10 - Building Front-end Applications with React/Jamming/jamming/src/components/TrackList/TrackList.js
+++ 10 - Building Front-end Applications with React/Jamming/jamming/src/components/TrackList/TrackList.js
@@ -10,15 +10,17 @@ import Track from "./../Track/Track";
 function TrackList(props) {
   return (
     <div className="TrackList">
-      {props.tracks.map(track =>
-          <Track
-          onAdd={props.onAdd}
-          onRemove={props.onRemove}
-          isRemoval={props.isRemoval}
-          key={track.id}
-          track={track}
+      {props.tracks.map(track => {
+          return (
+            <Track
+            onAdd={props.onAdd}
+            onRemove={props.onRemove}
+            isRemoval={props.isRemoval}
+            key={track.id}
+            track={track}
           />
-      )}
+          );
+      })}
     </div>
   );
 }
