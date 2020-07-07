024e58ac70a0b4264a27fac2469d2cecc73d86ba
dyrits <dyrits@outlook.fr>
2020-07-07 17:07:16 +0200

    Update (V0.83)

--- 10 - Building Front-end Applications with React/Jamming/jamming/src/util/Spotify.js
+++ 10 - Building Front-end Applications with React/Jamming/jamming/src/util/Spotify.js
@@ -1,8 +1,17 @@
-let userAccessToken;
+import {CLIENT_ID, REDIRECT_URI} from "./SpotifyAppSettings";
+let accessToken;
 
 const Spotify = {
   getAccessToken() {
-    if (userAccessToken) { return userAccessToken; }
+    if (accessToken) { return accessToken; }
+    accessToken = window.location.href.match(/access_token=([^&]*)/)[1];
+    const expiresIn = window.location.href.match(/expires_in=([^&]*)/)[1];
+    if (accessToken && expiresIn) {
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+      return accessToken;
+    }
+    window.location = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`;
   }
 }
 
