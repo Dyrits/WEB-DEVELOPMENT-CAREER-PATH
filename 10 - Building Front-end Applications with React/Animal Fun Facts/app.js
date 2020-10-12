1907c8231f019577be75ba0e6562f395130b7dd1
dyrits <dyrits@outlook.fr>
2020-10-12 16:12:19 +0300

    Minor update.

--- 10 - Building Front-end Applications with React/Animal Fun Facts/app.js
+++ 10 - Building Front-end Applications with React/Animal Fun Facts/app.js
@@ -1,6 +1,19 @@
-import {animals} from './animals';
-import React from 'react';
-import ReactDOM from 'react-dom';
+'use strict';
+
+const animals = {
+  dolphin: {
+    image: './images/dolphin.png',
+    facts: ['Dolphins have been shown to give distinct names to each other!', 'Dolphins are known to display their own culture!', 'Dolphins have two stomachs!']
+  },
+  lobster: {
+    image: './images/lobster.png',
+    facts: ['Lobsters taste with their legs!', 'Lobsters chew with their stomachs!', 'Lobsters can live as long as 100 years.']
+  },
+  starfish: {
+    image: './images/starfish.png',
+    facts: ['Starfish can have up to 40 arms!', 'Starfish have no brain and no blood!', 'Starfish can regenerate their own arms!']
+  }
+};
 
 const displayFact = $event => {
   const animal = animals[$event.target.alt]
@@ -27,7 +40,7 @@ const background = (
   <img
     className="backkground"
     alt="ocean"
-    src="/images/ocean.jpg"
+    src="./images/ocean.jpg"
   />
 );
 
@@ -41,6 +54,5 @@ const animalFacts = (
     <p id="fact"/>
   </div>
 );
-
 
 ReactDOM.render(animalFacts, document.getElementById('root'))
